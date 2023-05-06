d9c9db1e061d177cf421f806a922d18f41c194e8
Syaiful Anwar <syaiful.anwar@dana.id>
2023-05-06 16:39:11 +0700

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+
+export {
+  CustomButton,
+  FilePicker,
+  ColorPicker,
+  AIPicker,
+  Tab
+}
